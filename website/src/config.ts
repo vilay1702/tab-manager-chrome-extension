0727db51e7172eb84c0771c4e050b2cef1b97705
Vilay Bende <vilay17bende@gmail.com>
2026-05-22 21:41:56 +0530

    Update config.ts
    Added chrome extension id

--- website/src/config.ts
+++ website/src/config.ts
@@ -1,4 +1,5 @@
 // Single place to update store + contact info before launch.
+export const EXTENSION_ID="ajfbbiinmfamfajblcpbbhbojhbibmii";
 export const CHROME_STORE_URL =
-  'https://chrome.google.com/webstore/detail/REPLACE_WITH_EXTENSION_ID';
+  `https://chrome.google.com/webstore/detail/${EXTENSION_ID}`;
 export const SUPPORT_EMAIL = 'vilay17bende@gmail.com';
